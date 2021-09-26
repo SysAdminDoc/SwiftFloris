c5ecb272b31da92d59116e17a69e0c648a18a561
Patrick Goldinger <patrick@patrickgold.dev>
2021-09-27 00:28:13 +0200

    Add ThemeScreen (overview) to new Settings UI
    ThemeManager/Editor still in old UI

---
+++ app/src/main/java/dev/patrickgold/florisboard/app/ui/Routes.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2021 Patrick Goldinger
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.patrickgold.florisboard.app.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import dev.patrickgold.florisboard.app.ui.settings.HomeScreen
+import dev.patrickgold.florisboard.app.ui.settings.about.AboutScreen
+import dev.patrickgold.florisboard.app.ui.settings.about.ProjectLicenseScreen
+import dev.patrickgold.florisboard.app.ui.settings.about.ThirdPartyLicensesScreen
+import dev.patrickgold.florisboard.app.ui.settings.advanced.AdvancedScreen
+import dev.patrickgold.florisboard.app.ui.settings.clipboard.ClipboardScreen
+import dev.patrickgold.florisboard.app.ui.settings.gestures.GesturesScreen
+import dev.patrickgold.florisboard.app.ui.settings.keyboard.InputFeedbackScreen
+import dev.patrickgold.florisboard.app.ui.settings.keyboard.KeyboardScreen
+import dev.patrickgold.florisboard.app.ui.settings.theme.ThemeScreen
+import dev.patrickgold.florisboard.app.ui.setup.SetupScreen
+
+object Routes {
+    object Settings {
+        const val Home = "settings"
+
+        const val Theme = "settings/theme"
+
+        const val Keyboard = "settings/keyboard"
+        const val InputFeedback = "settings/keyboard/input-feedback"
+
+        const val Gestures = "settings/gestures"
+
+        const val Clipboard = "settings/clipboard"
+
+        const val Advanced = "settings/advanced"
+
+        const val About = "settings/about"
+        const val ProjectLicense = "settings/about/project-license"
+        const val ThirdPartyLicenses = "settings/about/third-party-licenses"
+    }
+
+    object Setup {
+        const val Home = "setup"
+    }
+
+    @Composable
+    fun AppNavHost(
+        modifier: Modifier,
+        navController: NavHostController,
+        startDestination: String,
+    ) {
+        NavHost(
+            modifier = modifier,
+            navController = navController,
+            startDestination = startDestination,
+        ) {
+            composable(Settings.Home) { HomeScreen() }
+
+            composable(Settings.Theme) { ThemeScreen() }
+
+            composable(Settings.Keyboard) { KeyboardScreen() }
+            composable(Settings.InputFeedback) { InputFeedbackScreen() }
+
+            composable(Settings.Gestures) { GesturesScreen() }
+
+            composable(Settings.Clipboard) { ClipboardScreen() }
+
+            composable(Settings.Advanced) { AdvancedScreen() }
+
+            composable(Settings.About) { AboutScreen() }
+            composable(Settings.ProjectLicense) { ProjectLicenseScreen() }
+            composable(Settings.ThirdPartyLicenses) { ThirdPartyLicensesScreen() }
+
+            composable(Setup.Home) { SetupScreen() }
+        }
+    }
+}
---
+++ app/src/main/java/dev/patrickgold/florisboard/ime/text/gestures/GlideTypingManager.kt
@@ -0,0 +1,130 @@
+package dev.patrickgold.florisboard.ime.text.gestures
+
+import dev.patrickgold.florisboard.app.prefs.florisPreferenceModel
+import dev.patrickgold.florisboard.ime.core.FlorisBoard
+import dev.patrickgold.florisboard.ime.core.Subtype
+import dev.patrickgold.florisboard.ime.nlp.SuggestionList
+import dev.patrickgold.florisboard.ime.text.TextInputManager
+import dev.patrickgold.florisboard.ime.text.keyboard.TextKey
+import dev.patrickgold.florisboard.res.AssetManager
+import dev.patrickgold.florisboard.res.FlorisRef
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import kotlin.math.min
+
+/**
+ * Handles the [GlideTypingClassifier]. Basically responsible for linking [GlideTypingGesture.Detector]
+ * with [GlideTypingClassifier].
+ */
+class GlideTypingManager : GlideTypingGesture.Listener, CoroutineScope by MainScope() {
+    private var glideTypingClassifier = StatisticalGlideTypingClassifier()
+    private val prefs by florisPreferenceModel()
+
+    companion object {
+        private const val MAX_SUGGESTION_COUNT = 8
+
+        private lateinit var glideTypingManager: GlideTypingManager
+        fun getInstance(): GlideTypingManager {
+            if (!this::glideTypingManager.isInitialized) {
+                glideTypingManager = GlideTypingManager()
+            }
+            return glideTypingManager
+        }
+    }
+
+    override fun onGlideComplete(data: GlideTypingGesture.Detector.PointerData) {
+        updateSuggestionsAsync(MAX_SUGGESTION_COUNT, true) {
+            glideTypingClassifier.clear()
+        }
+    }
+
+    override fun onGlideCancelled() {
+        glideTypingClassifier.clear()
+    }
+
+    private var lastTime = System.currentTimeMillis()
+    override fun onGlideAddPoint(point: GlideTypingGesture.Detector.Position) {
+        val normalized = GlideTypingGesture.Detector.Position(point.x, point.y)
+
+        this.glideTypingClassifier.addGesturePoint(normalized)
+
+        val time = System.currentTimeMillis()
+        if (prefs.glide.showPreview.get() && time - lastTime > prefs.glide.previewRefreshDelay.get()) {
+            updateSuggestionsAsync(1, false) {}
+            lastTime = time
+        }
+    }
+
+    /**
+     * Change the layout of the internal gesture classifier
+     */
+    fun setLayout(keys: List<TextKey>) {
+        glideTypingClassifier.setLayout(keys, FlorisBoard.getInstance().activeSubtype)
+    }
+
+    private val wordDataCache = hashMapOf<String, Int>()
+
+    /**
+     * Set the word data for the internal gesture classifier
+     */
+    fun setWordData(subtype: Subtype) {
+        launch(Dispatchers.Default) {
+            if (wordDataCache.isEmpty()) {
+                // FIXME: get this info from dictionary.
+                val data =
+                    AssetManager.default().loadTextAsset(FlorisRef.assets("ime/dict/data.json"))
+                        .getOrThrow()
+                val json = JSONObject(data)
+                wordDataCache.putAll(json.keys().asSequence().map { Pair(it, json.getInt(it)) })
+            }
+            glideTypingClassifier.setWordData(wordDataCache, subtype)
+        }
+    }
+
+    /**
+     * Asks gesture classifier for suggestions and then passes that on to the smartbar.
+     * Also commits the most confident suggestion if [commit] is set. All happens on an async executor.
+     * NB: only fetches [MAX_SUGGESTION_COUNT] suggestions.
+     *
+     * @param callback Called when this function completes. Takes a boolean, which indicates if suggestions
+     * were successfully set.
+     */
+    private fun updateSuggestionsAsync(maxSuggestionsToShow: Int, commit: Boolean, callback: (Boolean) -> Unit) {
+        if (!glideTypingClassifier.ready) {
+            callback.invoke(false)
+            return
+        }
+
+        launch(Dispatchers.Default) {
+            // To avoid cache misses when maxSuggestions goes from 5 to 1.
+            val time = System.nanoTime()
+            val suggestions = glideTypingClassifier.getSuggestions(MAX_SUGGESTION_COUNT, true)
+
+            withContext(Dispatchers.Main) {
+                val textInputManager = TextInputManager.getInstance()
+                textInputManager.isGlidePostEffect = true
+                val suggestionList = SuggestionList.new(1)
+                suggestions.subList(
+                    1.coerceAtMost(min(commit.compareTo(false), suggestions.size)),
+                    maxSuggestionsToShow.coerceAtMost(suggestions.size)
+                ).map { textInputManager.fixCase(it) }.forEach {
+                    suggestionList.add(it, 255)
+                }
+                textInputManager.smartbarView?.setCandidateSuggestionWords(
+                    time,
+                    suggestionList
+                )
+                suggestionList.dispose()
+                textInputManager.smartbarView?.updateCandidateSuggestionCapsState()
+                if (commit && suggestions.isNotEmpty()) {
+                    textInputManager.handleGesture(suggestions.first())
+                }
+                callback.invoke(true)
+            }
+        }
+    }
+}
