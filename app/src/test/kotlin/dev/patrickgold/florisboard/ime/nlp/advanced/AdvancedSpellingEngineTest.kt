ae66449dd4f03c773100a950209dd66afb67e13e
Matthew Parker <matt_parker@outlook.com>
2026-05-05 09:29:33 -0400

    test: cover advanced spelling engine

---
+++ app/src/test/kotlin/dev/patrickgold/florisboard/ime/nlp/advanced/AdvancedSpellingEngineTest.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2026 SwiftFloris Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.patrickgold.florisboard.ime.nlp.advanced
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class AdvancedSpellingEngineTest : FunSpec({
+    val dictionary = setOf(
+        "keyboard",
+        "keyboards",
+        "key",
+        "swift",
+        "shift",
+        "offline",
+    )
+
+    test("levenshteinDistance handles insertion deletion substitution and equality") {
+        AdvancedSpellingEngine.levenshteinDistance("swift", "swift") shouldBe 0
+        AdvancedSpellingEngine.levenshteinDistance("swift", "shift") shouldBe 1
+        AdvancedSpellingEngine.levenshteinDistance("keybord", "keyboard") shouldBe 1
+        AdvancedSpellingEngine.levenshteinDistance("", "key") shouldBe 3
+    }
+
+    test("generateCorrections returns deterministic edit-distance ranked suggestions") {
+        AdvancedSpellingEngine.generateCorrections("keybord", dictionary, maxCount = 3) shouldBe
+            listOf("keyboard", "keyboards")
+    }
+
+    test("generateCorrections filters distant words and respects max count") {
+        AdvancedSpellingEngine.generateCorrections("swft", dictionary, maxCount = 1) shouldBe listOf("swift")
+        AdvancedSpellingEngine.generateCorrections("zzzzzz", dictionary, maxCount = 3) shouldBe emptyList()
+    }
+})
